4be0208b004b145274c466184a94427dcb457a1f
Vitor <vitorkhaled47@gmail.com>
2024-01-31 20:06:04 -0300

    criando crud com scaffolding

---
+++ SalesWebMvc2019/Data/SalesWebMvc2019Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc2019.Models;
+
+namespace SalesWebMvc2019.Data
+{
+    public class SalesWebMvc2019Context : DbContext
+    {
+        public SalesWebMvc2019Context (DbContextOptions<SalesWebMvc2019Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvc2019.Models.Department> Department { get; set; }
+    }
+}
